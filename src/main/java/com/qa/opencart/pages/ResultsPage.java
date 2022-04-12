3c128bbebb5f1a0fb244d6f4c9542159cc1b90c7
Saranya <vbsaran@gmail.com>
2022-04-12 15:35:55 +0800

    Added By locator

--- src/main/java/com/qa/opencart/pages/ResultsPage.java
+++ src/main/java/com/qa/opencart/pages/ResultsPage.java
@@ -1,10 +1,13 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ResultsPage {
 	
 	private WebDriver driver;
+	
+	By resultsLink =By.linkText("ResultsLink");
 	
 	public ResultsPage(WebDriver driver)
 	{
