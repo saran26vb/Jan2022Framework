926a28093b310296be87b421b40b1da4233740e8
Saranya <vbsaran@gmail.com>
2022-04-12 15:30:23 +0800

    Added ResultsPage

---
+++ src/main/java/com/qa/opencart/pages/ResultsPage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ResultsPage {
+	
+	private WebDriver driver;
+	
+	public ResultsPage(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+
+}
